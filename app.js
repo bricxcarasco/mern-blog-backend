883768c5185d2568c87d5cdae9eba7b139903229
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-18 15:01:54 +0800

    Setup MongoDB connection and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const { MONGODB_URI } = require('./config/keys');
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGODB_URI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("MongoDB is connected!");
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`MongoDB connection error ${err}`);
+});
 
 app.get('/', (req, res) => {
     res.send("Hello!");
