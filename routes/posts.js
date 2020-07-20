c2c2bc6a18c687e19fc8ce1dabd94d0f67acc483
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-07-20 16:58:41 +0800

    Modify new post api add category link

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const Category = mongoose.model("Category");
 const Post = mongoose.model("Post");
 
 const router = express.Router();
@@ -19,26 +20,35 @@ router.get('/posts', (req, res) => {
 });
 
 router.post('/new-post', (req, res) => {
-    const { title, description, imageUrl } = req.body;
+    const { title, description, imageUrl, category } = req.body;
 
-    if (!title || !description || !imageUrl) {
+    if (!title || !description || !imageUrl || !category) {
         return res.status(422).json({
             error: "All fields are required"
         });
     }
 
-    const post = new Post({
-        title,
-        description,
-        imageUrl
-    });
-
-    post.save()
-        .then((post) => {
-            res.json({
-                message: "Post has been created",
-                post
+    Category.findOne({ 
+            _id: category._id
+        })
+        .then((category_result) => {
+            const post = new Post({
+                title,
+                description,
+                imageUrl,
+                category: category_result
             });
+        
+            post.save()
+                .then((post) => {
+                    res.json({
+                        message: "Post has been created",
+                        post
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         })
         .catch((error) => {
             console.log(error);
